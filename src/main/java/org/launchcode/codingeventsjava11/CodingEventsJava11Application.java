98cfeb850282d7c0348113974b41c18abf1573c3
rinn-code <98275792+rinn-code@users.noreply.github.com>
2022-10-04 20:38:37 -0500

    First Commit

---
+++ src/main/java/org/launchcode/codingeventsjava11/CodingEventsJava11Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingeventsjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEventsJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEventsJava11Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/codingeventsjava11/CodingEventsJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingeventsjava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
