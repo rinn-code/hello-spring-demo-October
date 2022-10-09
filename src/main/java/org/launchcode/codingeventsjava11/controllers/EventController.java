169d535656c5cec961fc0ac1102509f88c26720e
rinn-code <98275792+rinn-code@users.noreply.github.com>
2022-10-09 15:34:25 -0500

    14.4 complete

--- src/main/java/org/launchcode/codingeventsjava11/controllers/EventController.java
+++ src/main/java/org/launchcode/codingeventsjava11/controllers/EventController.java
@@ -5,10 +5,7 @@ import org.launchcode.codingeventsjava11.data.EventData;
 import org.launchcode.codingeventsjava11.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,8 +28,8 @@ public class EventController{
     }
     //lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
-        EventData.add(new Event(eventName, eventDescription));
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:";
     }
 
@@ -51,7 +48,6 @@ public class EventController{
                EventData.remove(id);
            }
        }
-
         return "redirect:";
     }
 
