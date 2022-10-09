30b02a52e65ea7641d51883a6202032496375cd4
rinn-code <98275792+rinn-code@users.noreply.github.com>
2022-10-09 15:48:48 -0500

    CH 14 exercises complete?

--- src/main/java/org/launchcode/codingeventsjava11/controllers/EventController.java
+++ src/main/java/org/launchcode/codingeventsjava11/controllers/EventController.java
@@ -14,16 +14,16 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController{
 
-
     @GetMapping
     public String displayAllEvents(Model model){
-            model.addAttribute("title", "All Events");
-            model.addAttribute("events", EventData.getAll());
-            return "events/index";
-            }
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/index";
+    }
     //lives at/events/create
     @GetMapping("create")
     public String displayCreateEventForm(){
+
         return "events/create";
     }
     //lives at /events/create
@@ -49,6 +49,21 @@ public class EventController{
            }
        }
         return "redirect:";
+    }
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title);
+        return "events/edit";
+    }
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect";
     }
 
 }
